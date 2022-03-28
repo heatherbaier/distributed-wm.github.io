7b6f86acecbb4981e2eb75dd26f71f20fa38b99b
heatherbaier <hmbaier@email.wm.edu>
2022-03-28 13:34:14 -0400

    sh scripts

---
+++ examples/tf_mwms/run.sh
@@ -0,0 +1,10 @@
+rm -R /sciclone/home20/hmbaier/tflow/ips/
+mkdir /sciclone/home20/hmbaier/tflow/ips/
+
+rm -R /sciclone/home20/hmbaier/tflow/logs/
+mkdir /sciclone/home20/hmbaier/tflow/logs/
+
+for ((i = 1; i <= $1; i++))
+do
+	qsub /sciclone/home20/hmbaier/tflow/tflow_job.sh -l nodes=1:$2:ppn=1 -v NODE_NUM=$i,WORLD_SIZE=$1
+done
